c32137d6eb45b5c4e200c4d8cff560f1fbfef36b
Ubuntu <ubuntu@ip-172-31-63-18.ec2.internal>
2021-02-14 09:09:34 +0000

    design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,9 @@ class UsersController < ApplicationController
   def show
     @books = @user.books.where(status: ['読書中', nil]).order(id: :desc).page(params[:page]).per(15)
     this_month_books(@user)
+    last_month_books(@user)
+    last_last_month_books(@user)
+    @data_for_graph = { "#{@last_last_month}月(先々月)": @last_last_month_books.count, "#{@last_month}月(先月)": @last_month_books.count, "#{@this_month}月(今月)": @this_month_books.count }
   end
 
   def will_read
